82f46c7433895c9e488d756978549e0b19b3960f
Zerong Li <v-zerongli@microsoft.com>
2024-02-28 18:30:49 +0800

    chapter 12: confused with function in client component

---
+++ app/lib/actions.ts
@@ -0,0 +1,11 @@
+// 'use server';
+ 
+export async function createInvoice(formData: any) {
+  const rawFormData = {
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  };
+  // Test it out:
+  console.log(rawFormData);
+}
